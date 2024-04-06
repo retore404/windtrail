a2bf01958baa43217915d46290d361ffc3446c70
retore404 <44674058+retore404@users.noreply.github.com>
2024-04-06 16:37:55 +0900

    add: npm-run-all and typed-css-modules

---
+++ app/src/app/page.module.css.d.ts
@@ -0,0 +1,14 @@
+declare const styles: {
+  readonly "card": string;
+  readonly "center": string;
+  readonly "code": string;
+  readonly "content": string;
+  readonly "description": string;
+  readonly "grid": string;
+  readonly "logo": string;
+  readonly "main": string;
+  readonly "rotate": string;
+  readonly "vercelLogo": string;
+};
+export = styles;
+
