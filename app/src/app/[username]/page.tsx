063f7fab71494dacaf50619ebbc12e86f4400541
retore404 <44674058+retore404@users.noreply.github.com>
2024-04-07 23:03:49 +0900

    add: usernameからdidを取得し表示するサンプル

--- app/src/app/[username]/page.tsx
+++ app/src/app/[username]/page.tsx
@@ -1,9 +1,25 @@
-export default function Page({
+export default async function Page({
   params,
   searchParams,
 }: {
   params: { username: string };
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
-  return <h1>{params.username}</h1>;
+  const res = await fetch(
+    `https://public.api.bsky.app/xrpc/com.atproto.identity.resolveHandle?handle=` +
+      params.username,
+    {
+      next: { revalidate: 3600 },
+    },
+  );
+  if (res.status != 200) {
+    return <span>Could not get user data.</span>;
+  }
+  const data = await res.json();
+  return (
+    <section>
+      <h1>{params.username}</h1>
+      <span>{data.did}</span>
+    </section>
+  );
 }
--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main>
-      <Link href={`/a`}>a</Link>
+      <Link href={`/retore.bsky.social`}>retore.bsky.social</Link>
     </main>
   );
 }
