bfb5acc155224ea99e87c1ab624f5c0fd2b23c52
retore404 <44674058+retore404@users.noreply.github.com>
2024-04-08 12:36:49 +0900

    update: metadata動的生成

--- app/src/app/[username]/page.tsx
+++ app/src/app/[username]/page.tsx
@@ -1,10 +1,29 @@
-export default async function Page({
-  params,
-  searchParams,
-}: {
+import { Metadata, ResolvingMetadata } from "next";
+
+// 引数の型定義
+type Props = {
   params: { username: string };
   searchParams: { [key: string]: string | string[] | undefined };
-}) {
+};
+
+// メタデータを生成
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  const username = params.username;
+
+  return {
+    title: username + " | Windtrail",
+    description: username + "",
+    openGraph: {
+      title: username + " | Windtrail",
+      description: username + "",
+    },
+  };
+}
+
+export default async function Page({ params, searchParams }: Props) {
   const res = await fetch(
     `https://public.api.bsky.app/xrpc/com.atproto.identity.resolveHandle?handle=` +
       params.username,
--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,5 +1,11 @@
 import Link from "next/link";
 import styles from "./page.module.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Windtrail2",
+  description: "Logging posts on Bluesky Social.",
+};
 
 export default function Home() {
   return (
