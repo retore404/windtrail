eebe32def11d466f07fb0ecf0984a2798e725b40
retore404 <44674058+retore404@users.noreply.github.com>
2024-04-08 21:14:44 +0900

    close: ユーザ情報表示

--- app/src/app/[username]/page.tsx
+++ app/src/app/[username]/page.tsx
@@ -1,4 +1,13 @@
-import { Card, CardContent, Typography } from "@mui/material";
+import {
+  Avatar,
+  Card,
+  CardContent,
+  Divider,
+  Stack,
+  StyledEngineProvider,
+  Typography,
+  styled,
+} from "@mui/material";
 import { Metadata, ResolvingMetadata } from "next";
 
 // 引数の型定義
@@ -25,22 +34,70 @@ export async function generateMetadata(
 }
 
 export default async function Page({ params, searchParams }: Props) {
-  const res = await fetch(
+  // didの取得
+  const didRes = await fetch(
     `https://public.api.bsky.app/xrpc/com.atproto.identity.resolveHandle?handle=` +
       params.username,
     {
       next: { revalidate: 3600 },
     },
   );
-  if (res.status != 200) {
-    return <span>Could not get user data.</span>;
+  if (didRes.status != 200) {
+    return <span>Could not get did.</span>;
   }
-  const data = await res.json();
+  const didData = await didRes.json();
+  const did = didData.did;
+
+  // ユーザ情報の取得
+  const userInfoRes = await fetch(
+    `https://public.api.bsky.app/xrpc/app.bsky.actor.getProfile?actor=` + did,
+    {
+      next: { revalidate: 3600 },
+    },
+  );
+
+  if (userInfoRes.status != 200) {
+    return <span>Could not get user profile.</span>;
+  }
+
+  const userData = await userInfoRes.json();
+
+  const displayName = userData.displayName;
+  const avatarImage = userData.avatar;
+  const description = userData.description;
+  const followersCount = userData.followersCount;
+  const followsCount = userData.followsCount;
+  const postsCount = userData.postsCount;
+
   return (
     <Card>
       <CardContent>
-        <Typography variant="h4">{params.username}</Typography>
-        <span>{data.did}</span>
+        <Stack>
+          <Stack direction="row" spacing={1}>
+            <Avatar alt="Cindy Baker" src={avatarImage} />
+            <Typography variant="h4" component="h2">
+              {displayName}
+            </Typography>
+          </Stack>
+          <Typography>
+            {`@` + params.username} / {did}
+          </Typography>
+          <Typography>
+            {postsCount} posts. / Follows: {followsCount} / Followers:{" "}
+            {followersCount}
+          </Typography>
+        </Stack>
+        <StyledEngineProvider injectFirst>
+          <Divider
+            sx={{
+              marginTop: "0.5em",
+              marginBottom: "0.5em",
+            }}
+          />
+        </StyledEngineProvider>
+        <Stack>
+          <Typography>{description}</Typography>
+        </Stack>
       </CardContent>
     </Card>
   );
