a2693d4db1d1a113fce5e31f4ca2502e1800cf36
retore404 <44674058+retore404@users.noreply.github.com>
2024-04-16 12:01:40 +0900

    ユーザ情報表示のコンポーネント化

--- app/src/app/[username]/page.tsx
+++ app/src/app/[username]/page.tsx
@@ -1,14 +1,5 @@
-import {
-  Avatar,
-  Card,
-  CardContent,
-  Divider,
-  Stack,
-  StyledEngineProvider,
-  Typography,
-  styled,
-} from "@mui/material";
 import { Metadata, ResolvingMetadata } from "next";
+import UserInfo from "../_component/UserInfo";
 
 // 引数の型定義
 type Props = {
@@ -34,71 +25,7 @@ export async function generateMetadata(
 }
 
 export default async function Page({ params, searchParams }: Props) {
-  // didの取得
-  const didRes = await fetch(
-    `https://public.api.bsky.app/xrpc/com.atproto.identity.resolveHandle?handle=` +
-      params.username,
-    {
-      next: { revalidate: 3600 },
-    },
-  );
-  if (didRes.status != 200) {
-    return <span>Could not get did.</span>;
-  }
-  const didData = await didRes.json();
-  const did = didData.did;
-
-  // ユーザ情報の取得
-  const userInfoRes = await fetch(
-    `https://public.api.bsky.app/xrpc/app.bsky.actor.getProfile?actor=` + did,
-    {
-      next: { revalidate: 3600 },
-    },
-  );
-
-  if (userInfoRes.status != 200) {
-    return <span>Could not get user profile.</span>;
-  }
-
-  const userData = await userInfoRes.json();
-
-  const displayName = userData.displayName;
-  const avatarImage = userData.avatar;
-  const description = userData.description;
-  const followersCount = userData.followersCount;
-  const followsCount = userData.followsCount;
-  const postsCount = userData.postsCount;
-
   return (
-    <Card>
-      <CardContent>
-        <Stack>
-          <Stack direction="row" spacing={1}>
-            <Avatar alt="Cindy Baker" src={avatarImage} />
-            <Typography variant="h4" component="h2">
-              {displayName}
-            </Typography>
-          </Stack>
-          <Typography>
-            {`@` + params.username} / {did}
-          </Typography>
-          <Typography>
-            {postsCount} posts. / Follows: {followsCount} / Followers:{" "}
-            {followersCount}
-          </Typography>
-        </Stack>
-        <StyledEngineProvider injectFirst>
-          <Divider
-            sx={{
-              marginTop: "0.5em",
-              marginBottom: "0.5em",
-            }}
-          />
-        </StyledEngineProvider>
-        <Stack>
-          <Typography>{description}</Typography>
-        </Stack>
-      </CardContent>
-    </Card>
+    <UserInfo params={{username: params.username}}></UserInfo>
   );
 }
