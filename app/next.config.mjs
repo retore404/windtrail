c28de1d2591ac4b7f84ab6151891585e28c8227a
retore404 <44674058+retore404@users.noreply.github.com>
2024-05-18 07:05:29 +0900

    close #42: 埋め込み画像表示

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.bsky.app",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
